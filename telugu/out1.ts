6f80a12ae0373486f7e110643d38de25b00e7540
vicky1111000 <165231338+vaathala00@users.noreply.github.com>
2024-04-19 15:17:44 +0530

    Files upload

--- telugu/out1.ts
+++ telugu/out1.ts
@@ -2,7 +2,7 @@
 #EXT-X-TARGETDURATION:10
 #EXT-X-VERSION:3
 #EXTINF:10,
-http://zeestb1.online/ditto.php?id=2577
+http://tv.cloudcdn.me:80/00:1A:79:07:8F:5A/1697420463/288
 #EXTINF:10,
 http://tv.cloudcdn.me:80/00:1A:79:07:8F:5A/1697420463/288
 
