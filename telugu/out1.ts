07f1cb21a1123fafad35ea29d9c673e516d48a5a
vicky1111000 <165231338+vaathala00@users.noreply.github.com>
2024-04-30 07:44:45 +0530

    Update out1.ts

--- telugu/out1.ts
+++ telugu/out1.ts
@@ -2,7 +2,7 @@
 #EXT-X-TARGETDURATION:10
 #EXT-X-VERSION:3
 #EXTINF:10,
-http://tv.cloudcdn.me:80/00:1A:79:07:8F:5A/1697420463/288
+http://tv.cloudcdn.me/live.ts?channelId=288&uid=4280&deviceMac=00:1A:79:17:C1:7F
 #EXTINF:10,
-http://tv.cloudcdn.me:80/00:1A:79:07:8F:5A/1697420463/288
+http://tv.cloudcdn.me/live.ts?channelId=288&uid=4280&deviceMac=00:1A:79:17:C1:7F
 
