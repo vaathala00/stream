30b35fca0c899506cb9c307bf6914b9c5a2d2069
vicky1111000 <165231338+vaathala00@users.noreply.github.com>
2024-05-25 17:10:46 +0530

    Update out1.ts

--- telugu/out1.ts
+++ telugu/out1.ts
@@ -2,7 +2,6 @@
 #EXT-X-TARGETDURATION:10
 #EXT-X-VERSION:3
 #EXTINF:10,
-http://tv.cloudcdn.me/live.ts?channelId=288&uid=4280&deviceMac=00:1A:79:17:C1:7F
+https://toxicify.pro/tsex/jio.m3u8?id=8457&e=.m3u8
 #EXTINF:10,
-http://tv.cloudcdn.me/live.ts?channelId=288&uid=4280&deviceMac=00:1A:79:17:C1:7F
-
+https://toxicify.pro/tsex/jio.m3u8?id=8457&e=.m3u8
