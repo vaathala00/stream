5f200a98b7045f1553f33f3bbb90b890df180a63
vicky1111000 <165231338+vaathala00@users.noreply.github.com>
2024-05-26 17:14:15 +0530

    Files upload

--- telugu/out1.ts
+++ telugu/out1.ts
@@ -2,6 +2,6 @@
 #EXT-X-TARGETDURATION:10
 #EXT-X-VERSION:3
 #EXTINF:10,
-https://toxicify.pro/tsex/jio.m3u8?id=8457&e=.m3u8
+http://starshare.live:8080/live/35858547/86368536/177843.ts
 #EXTINF:10,
-https://toxicify.pro/tsex/jio.m3u8?id=8457&e=.m3u8
+http://tv.cloudcdn.me:80/live.ts?channelId=288&uid=1868&deviceMac=00:1A:79:94:98:FE
