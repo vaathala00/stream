4f1fffcde33242c5cb9873d4e61ec87e26031b6d
vicky1111000 <165231338+vaathala00@users.noreply.github.com>
2024-06-14 11:07:36 +0530

    Files upload

--- hungama/out1.ts
+++ hungama/out1.ts
@@ -2,6 +2,6 @@
 #EXT-X-TARGETDURATION:10
 #EXT-X-VERSION:3
 #EXTINF:10,
-http://tgaadi-web.tech/play.php?id=22175
+http://starshare.live:8080/live/Xw2fdKU6e8/XRgneaygHd/18456.ts
 #EXTINF:10,
-http://tgaadi-web.tech/play.php?id=22175
+http://starshare.live:8080/live/Xw2fdKU6e8/XRgneaygHd/18456.ts
--- vjmusic/out1.ts
+++ vjmusic/out1.ts
@@ -2,6 +2,8 @@
 #EXT-X-TARGETDURATION:10
 #EXT-X-VERSION:3
 #EXTINF:10,
-http://starshare.live:8080/live/35858547/86368536/23869.ts
+http://starshare.live:8080/live/Xw2fdKU6e8/XRgneaygHd/23869.ts
 #EXTINF:10,
-http://starshare.live:8080/live/35858547/86368536/23869.ts
+http://starshare.live:8080/live/Xw2fdKU6e8/XRgneaygHd/23869.ts
+
+
