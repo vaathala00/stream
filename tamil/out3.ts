63f330e1367a366809a6d8f8c0cdb041cd1e8034
vicky1111000 <165231338+vaathala00@users.noreply.github.com>
2024-07-06 16:16:11 +0530

    Files upload

--- tamil/out3.ts
+++ tamil/out3.ts
@@ -1,8 +1,7 @@
 #EXTM3U
 #EXT-X-TARGETDURATION:10
 #EXT-X-VERSION:3
-#EXT-X-PLAYLIST-TYPE:EVENT
 #EXTINF:10, BANDWIDTH=3000000, FRAME-RATE=60.0, AUDIO="5.1", CODECS="avc1.640028,ec-3.1.1,mp4a.40.5",
-https://line.4k-beast.co/EvxkSeprjOMKMpF/yzTJWtWT9G/143604
+http://dm.lion-ott.com/play/live.php?mac=00:1A:79:B0:97:AF&stream=281637&extension=ts
 #EXTINF:10, BANDWIDTH=3000000, FRAME-RATE=60.0, AUDIO="5.1", CODECS="avc1.640028,ec-3.1.1,mp4a.40.5",
-https://line.4k-beast.co/EvxkSeprjOMKMpF/yzTJWtWT9G/143604
+http://dm.lion-ott.com/play/live.php?mac=00:1A:79:B0:97:AF&stream=281637&extension=ts
