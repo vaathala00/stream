a85b364e83c5af69568fe842a028b7da378b9c69
vicky1111000 <165231338+vaathala00@users.noreply.github.com>
2024-07-08 17:22:46 +0530

    Files upload

---
+++ hindi/out2.ts
@@ -0,0 +1,7 @@
+#EXTM3U
+#EXT-X-TARGETDURATION:10
+#EXT-X-VERSION:3
+#EXTINF:10,
+http://proxym3u.watchtv.best:80/play/live.php?mac=00:1A:79:64:DD:34&stream=1114582&extension=ts
+#EXTINF:10,
+http://proxym3u.watchtv.best:80/play/live.php?mac=00:1A:79:64:DD:34&stream=1114582&extension=ts
--- tamil/out0.ts
+++ tamil/out0.ts
@@ -2,6 +2,6 @@
 #EXT-X-TARGETDURATION:10
 #EXT-X-VERSION:3
 #EXTINF:10, BANDWIDTH=3000000, FRAME-RATE=60.0, AUDIO="5.1", CODECS="avc1.640028,ec-3.1.1,mp4a.40.5",
-http://dm.lion-ott.com/play/live.php?mac=00:1A:79:B0:97:AF&stream=281637&extension=ts
+http://proxym3u.watchtv.best:80/play/live.php?mac=00:1A:79:64:DD:34&stream=155873&extension=ts
 #EXTINF:10, BANDWIDTH=3000000, FRAME-RATE=60.0, AUDIO="5.1", CODECS="avc1.640028,ec-3.1.1,mp4a.40.5",
-http://dm.lion-ott.com/play/live.php?mac=00:1A:79:B0:97:AF&stream=281637&extension=ts
+http://proxym3u.watchtv.best:80/play/live.php?mac=00:1A:79:64:DD:34&stream=155873&extension=ts
