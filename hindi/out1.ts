acc6fd6231834b992be37fc4d7be09654d36e12c
vicky1111000 <165231338+vaathala00@users.noreply.github.com>
2024-10-18 20:01:04 +0530

    Files upload

--- hindi/out1.ts
+++ hindi/out1.ts
@@ -2,6 +2,6 @@
 #EXT-X-TARGETDURATION:10
 #EXT-X-VERSION:3
 #EXTINF:10,
-http://sansat.net:88/02023053116090/7IBN86O8YXG95PQ/93725
+http://proxym3u.watchtv.best:80/play/live.php?mac=00:1A:79:64:DD:34&stream=155967&extension=ts&play_token=Bvx0lQgHcn
 #EXTINF:10,
-http://sansat.net:88/02023053116090/7IBN86O8YXG95PQ/93725
+http://proxym3u.watchtv.best:80/play/live.php?mac=00:1A:79:64:DD:34&stream=155967&extension=ts&play_token=Bvx0lQgHcn
--- hindi/out2.ts
+++ hindi/out2.ts
@@ -2,6 +2,6 @@
 #EXT-X-TARGETDURATION:10
 #EXT-X-VERSION:3
 #EXTINF:10,
-http://proxym3u.watchtv.best:80/play/live.php?mac=00:1A:79:64:DD:34&stream=1114582&extension=ts
+http://proxym3u.watchtv.best:80/play/live.php?mac=00:1A:79:64:DD:34&stream=155967&extension=ts&play_token=Bvx0lQgHcn
 #EXTINF:10,
-http://proxym3u.watchtv.best:80/play/live.php?mac=00:1A:79:64:DD:34&stream=1114582&extension=ts
+http://proxym3u.watchtv.best:80/play/live.php?mac=00:1A:79:64:DD:34&stream=155967&extension=ts&play_token=Bvx0lQgHcn
