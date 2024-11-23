cff580c0f959bc98ad0e085bb1850e18fcf5f339
vaathala00 <165231338+vaathala00@users.noreply.github.com>
2024-11-23 09:04:09 +0530

    Fies upload

--- tamil/out1.ts
+++ tamil/out1.ts
@@ -2,6 +2,6 @@
 #EXT-X-TARGETDURATION:10
 #EXT-X-VERSION:3
 #EXTINF:10, BANDWIDTH=3000000, FRAME-RATE=60.0, AUDIO="5.1", CODECS="avc1.640028,ec-3.1.1,mp4a.40.5",
-http://proxym3u.watchtv.best:80/play/live.php?mac=00:1A:79:64:DD:34&stream=155873&extension=ts&play_token=XxmZ2ZKtvX
+http://extraott.com:900/live/9976612136/9725477748/400159598.ts
 #EXTINF:10, BANDWIDTH=3000000, FRAME-RATE=60.0, AUDIO="5.1", CODECS="avc1.640028,ec-3.1.1,mp4a.40.5",
-http://proxym3u.watchtv.best:80/play/live.php?mac=00:1A:79:64:DD:34&stream=155873&extension=ts&play_token=XxmZ2ZKtvX
+http://extraott.com:900/live/9976612136/9725477748/400159598.ts
