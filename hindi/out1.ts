f35fd17f0bd050b8b71fd00ef37d37f9299a9335
vaathala00 <165231338+vaathala00@users.noreply.github.com>
2024-11-23 09:05:49 +0530

    Files upload

--- hindi/out1.ts
+++ hindi/out1.ts
@@ -2,6 +2,6 @@
 #EXT-X-TARGETDURATION:10
 #EXT-X-VERSION:3
 #EXTINF:10,
-http://proxym3u.watchtv.best:80/play/live.php?mac=00:1A:79:64:DD:34&stream=155967&extension=ts&play_token=Bvx0lQgHcn
+http://extraott.com:900/live/9976612136/9725477748/400159571.ts
 #EXTINF:10,
-http://proxym3u.watchtv.best:80/play/live.php?mac=00:1A:79:64:DD:34&stream=155967&extension=ts&play_token=Bvx0lQgHcn
+http://extraott.com:900/live/9976612136/9725477748/400159571.ts
