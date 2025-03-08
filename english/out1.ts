5d93af9f48b8a6e71b9e1bf4b14721ca925c3ede
vaathala00 <165231338+vaathala00@users.noreply.github.com>
2025-03-08 15:10:10 +0530

    Files upload

--- english/out1.ts
+++ english/out1.ts
@@ -2,6 +2,6 @@
 #EXT-X-TARGETDURATION:10
 #EXT-X-VERSION:3
 #EXTINF:10,
-http://suprino.xyz//PMF3J9X/BV7HG8H/1090635
+http://mag.4k365.xyz:80/play/live.php?mac=00:1A:79:60:78:40&stream=1114580&extension=ts&play_token=7cYWRkTZlx
 #EXTINF:10,
-http://suprino.xyz//PMF3J9X/BV7HG8H/1090635
+http://mag.4k365.xyz:80/play/live.php?mac=00:1A:79:60:78:40&stream=1114580&extension=ts&play_token=7cYWRkTZlx
