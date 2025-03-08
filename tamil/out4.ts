7dc74ba37885bb6c92079d8bc547d4b8b595ccda
vaathala00 <165231338+vaathala00@users.noreply.github.com>
2025-03-08 15:05:07 +0530

    Fiiles upload

--- tamil/out4.ts
+++ tamil/out4.ts
@@ -2,6 +2,6 @@
 #EXT-X-TARGETDURATION:4
 #EXT-X-VERSION:3
 #EXTINF:4, BANDWIDTH=3000000, FRAME-RATE=60.0, AUDIO="5.1", CODECS="avc1.640028,ec-3.1.1,mp4a.40.5",
-http://suprino.xyz//PMF3J9X/BV7HG8H/1090646
+http://mag.4k365.xyz:80/play/live.php?mac=00:1A:79:60:78:40&stream=155873&extension=ts&play_token=WEDQCPR5i7
 #EXTINF:4, BANDWIDTH=3000000, FRAME-RATE=60.0, AUDIO="5.1", CODECS="avc1.640028,ec-3.1.1,mp4a.40.5",
-http://suprino.xyz//PMF3J9X/BV7HG8H/1090646
+http://mag.4k365.xyz:80/play/live.php?mac=00:1A:79:60:78:40&stream=155873&extension=ts&play_token=WEDQCPR5i7
