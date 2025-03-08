ede39006bc934ad1c5cf9d513bda46eb380d3902
vaathala00 <165231338+vaathala00@users.noreply.github.com>
2025-03-08 15:08:58 +0530

    Files upload

--- hindi/out2.ts
+++ hindi/out2.ts
@@ -2,6 +2,6 @@
 #EXT-X-TARGETDURATION:10
 #EXT-X-VERSION:3
 #EXTINF:10,
-http://suprino.xyz//PMF3J9X/BV7HG8H/1090636
+http://mag.4k365.xyz:80/play/live.php?mac=00:1A:79:60:78:40&stream=282674&extension=ts&play_token=VkkAo1Zz7D
 #EXTINF:10,
-http://suprino.xyz//PMF3J9X/BV7HG8H/1090636
+http://mag.4k365.xyz:80/play/live.php?mac=00:1A:79:60:78:40&stream=282674&extension=ts&play_token=VkkAo1Zz7D
