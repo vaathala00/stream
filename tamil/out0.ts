d24ba1ad5c08478c9ece2fbbc2d7f2706d57fa1b
vaathala00 <165231338+vaathala00@users.noreply.github.com>
2025-03-13 08:29:39 +0530

    Files upload

--- tamil/out0.ts
+++ tamil/out0.ts
@@ -2,6 +2,6 @@
 #EXT-X-TARGETDURATION:4
 #EXT-X-VERSION:3
 #EXTINF:4, BANDWIDTH=3000000, FRAME-RATE=60.0, AUDIO="5.1", CODECS="avc1.640028,ec-3.1.1,mp4a.40.5",
-https://livetvbox.live:443/live/rajnikant/rajnikant/9722.ts
+https://livetvbox.live:443/live/rajnikant/rajnikant/9722.m3u8
 #EXTINF:4, BANDWIDTH=3000000, FRAME-RATE=60.0, AUDIO="5.1", CODECS="avc1.640028,ec-3.1.1,mp4a.40.5",
-https://livetvbox.live:443/live/rajnikant/rajnikant/9722.ts
+https://livetvbox.live:443/live/rajnikant/rajnikant/9722.m3u8
