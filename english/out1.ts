439ffb91156cb731bd815b0a9de752d07d5c9e71
vaathala00 <165231338+vaathala00@users.noreply.github.com>
2025-03-16 18:35:26 +0530

    Files upload

--- english/out1.ts
+++ english/out1.ts
@@ -2,6 +2,6 @@
 #EXT-X-TARGETDURATION:10
 #EXT-X-VERSION:3
 #EXTINF:10,
-https://livetvbox.live:443/live/rajnikant/rajnikant/481408.ts
+https://livetvbox.live:443/live/rajnikant/rajnikant/148.ts
 #EXTINF:10,
-https://livetvbox.live:443/live/rajnikant/rajnikant/481408.ts
+https://livetvbox.live:443/live/rajnikant/rajnikant/148.ts
--- tamil/out3.ts
+++ tamil/out3.ts
@@ -2,5 +2,6 @@
 #EXT-X-TARGETDURATION:4
 #EXT-X-VERSION:3
 #EXTINF:4, BANDWIDTH=3000000, FRAME-RATE=60.0, AUDIO="5.1", CODECS="avc1.640028,ec-3.1.1,mp4a.40.5",
-http://alpha-2ott.me:80/play/live.php?mac=00:1A:79:68:01:1A&stream=155873&extension=ts&play_token=aMBlKz5pd5#EXTINF:4, BANDWIDTH=3000000, FRAME-RATE=60.0, AUDIO="5.1", CODECS="avc1.640028,ec-3.1.1,mp4a.40.5",
-http://alpha-2ott.me:80/play/live.php?mac=00:1A:79:68:01:1A&stream=155873&extension=ts&play_token=aMBlKz5pd5
+http://alpha-2ott.me:80/play/live.php?mac=00:1A:79:68:01:1A&stream=155873&extension=ts
+#EXTINF:4, BANDWIDTH=3000000, FRAME-RATE=60.0, AUDIO="5.1", CODECS="avc1.640028,ec-3.1.1,mp4a.40.5",
+http://alpha-2ott.me:80/play/live.php?mac=00:1A:79:68:01:1A&stream=155873&extension=ts
