618a749e2b6d0805552688d9c3c96dba56d460cc
vaathala00 <165231338+vaathala00@users.noreply.github.com>
2025-03-22 17:44:26 +0530

    Files upload

--- english/out1.ts
+++ english/out1.ts
@@ -2,6 +2,6 @@
 #EXT-X-TARGETDURATION:10
 #EXT-X-VERSION:3
 #EXTINF:10,
-https://livetvbox.live:443//live/rajnikant/rajnikant/98864.ts
+https://livetvbox.live:443/live/rajnikant/rajnikant/98864.ts
 #EXTINF:10,
-https://livetvbox.live:443//live/rajnikant/rajnikant/98864.ts
+https://livetvbox.live:443/live/rajnikant/rajnikant/98864.ts
