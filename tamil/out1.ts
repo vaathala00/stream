6061d15470f4ebb00068026565ac537695ed5c2d
vaathala00 <165231338+vaathala00@users.noreply.github.com>
2025-03-26 07:18:16 +0530

    Files upload

--- tamil/out1.ts
+++ tamil/out1.ts
@@ -3,6 +3,6 @@
 #EXT-X-MEDIA-SEQUENCE:9722
 #EXT-X-VERSION:3
 #EXTINF:4, FRAME-RATE=60.0, AUDIO="5.1", CODECS="avc1.640028,ec-3.1.1,mp4a.40.5",
-https://002.fclplayer.online/live/csstream3/chunklist.ts?vidictid=201727157739&id=1003&pk=8b3ad8fb61061bfe4edb51cb9ccb21855127df9484799f597d947e7fa28af62a
-#EXTINF:4, BANDWIDTH=3000000, FRAME-RATE=60.0, AUDIO="5.1", CODECS="avc1.640028,ec-3.1.1,mp4a.40.5",
-https://002.fclplayer.online/live/csstream3/chunklist.ts?vidictid=201727157739&id=1003&pk=8b3ad8fb61061bfe4edb51cb9ccb21855127df9484799f597d947e7fa28af62a
+https://livetvbox.live:443/live/rajnikant/rajnikant/9722.ts
+#EXTINF:4, FRAME-RATE=60.0, AUDIO="5.1", CODECS="avc1.640028,ec-3.1.1,mp4a.40.5",
+https://livetvbox.live:443/live/rajnikant/rajnikant/9722.ts
