3da16cb3d20a42039557c8b36e6ab099043e632d
vaathala00 <165231338+vaathala00@users.noreply.github.com>
2025-03-28 18:29:40 +0530

    Files upload

--- hindi/out2.ts
+++ hindi/out2.ts
@@ -1,7 +1,7 @@
 #EXTM3U
 #EXT-X-TARGETDURATION:10
 #EXT-X-VERSION:3
-#EXT-X-START:TIME-OFFSET=0.0
+#EXT-X-MEDIA-SEQUENCE:89027
 
 #EXTINF:10,
 https://livetvbox.live:443/live/rajnikant/rajnikant/98866.ts
