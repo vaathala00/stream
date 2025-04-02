79e826c9dc2114ac71f49b89998a331c6e971af3
vaathala00 <165231338+vaathala00@users.noreply.github.com>
2025-04-02 17:48:44 +0530

    Update out1.ts

--- english/out1.ts
+++ english/out1.ts
@@ -11,6 +11,6 @@ http://line.now-ott.xyz:80/2D6D40/23CD52/63541
 #EXT-X-DISCONTINUITY
 
 #EXTINF:10, FRAME-RATE=60.0, AUDIO="5.1", CODECS="avc1.640028,ec-3.1.1,mp4a.40.5",
-http://line.now-ott.xyz:80/2D6D40/23CD
-52/63541#EXTINF:10, FRAME-RATE=60.0, AUDIO="5.1", CODECS="avc1.640028,ec-3.1.1,mp4a.40.5",
+http://line.now-ott.xyz:80/2D6D40/23CD52/63541
+#EXTINF:10, FRAME-RATE=60.0, AUDIO="5.1", CODECS="avc1.640028,ec-3.1.1,mp4a.40.5",
 http://line.now-ott.xyz:80/2D6D40/23CD52/63541
