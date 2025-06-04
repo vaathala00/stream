9e1c9e1ac5b500d19b6da8ecd808d3666f171bd7
vaathala00 <165231338+vaathala00@users.noreply.github.com>
2025-06-04 08:26:09 +0530

    Files upload

--- tamil/out4.ts
+++ tamil/out4.ts
@@ -1,14 +1,7 @@
 #EXTM3U
 #EXT-X-VERSION:3
 #EXT-X-TARGETDURATION:10
-#EXT-X-MEDIA-SEQUENCE:63537
-
 #EXTINF:10, FRAME-RATE=60.0, AUDIO="5.1", CODECS="avc1.640028,ec-3.1.1,mp4a.40.5",
-http://line.now-ott.xyz/2D6D40/23CD52/63538
+https://host.vodep39240327.workers.dev/out2.ts
 #EXTINF:10, FRAME-RATE=60.0, AUDIO="5.1", CODECS="avc1.640028,ec-3.1.1,mp4a.40.5",
-http://line.now-ott.xyz/2D6D40/23CD52/63538
-#EXT-X-DISCONTINUITY
-#EXTINF:10, FRAME-RATE=60.0, AUDIO="5.1", CODECS="avc1.640028,ec-3.1.1,mp4a.40.5",
-http://line.now-ott.xyz/2D6D40/23CD52/63538
-#EXTINF:10, FRAME-RATE=60.0, AUDIO="5.1", CODECS="avc1.640028,ec-3.1.1,mp4a.40.5",
-http://line.now-ott.xyz/2D6D40/23CD52/63538
+https://host.vodep39240327.workers.dev/out2.ts
